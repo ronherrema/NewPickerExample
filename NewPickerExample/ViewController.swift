2ef0e791fe475b32c0ed40e1014a686d1c8ea3ea
Ron Herrema <rherrema@gmail.com>
2019-11-18 22:48:15 +0000

    First commit.

--- NewPickerExample/ViewController.swift
+++ NewPickerExample/ViewController.swift
@@ -8,13 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    var colors = ["red", "green", "yellow", "purple", "orange", "pink", "lavender", "aqua", "rose", "rust"]
+    var types = ["rose", "violet", "lily", "peony", "carnation", "daffodil", "iris", "orchid", "azalea", "begonia"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        flowerChoice.delegate = self
+        flowerChoice.dataSource = self
     }
-
-
+    
+    // number of columns
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    // number of rows per column
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return colors.count
+        } else {
+            return types.count
+        }
+    }
+    
+    // updates the picker display
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return colors[row]
+        } else {
+            return types[row]
+        }
+    }
+    
+    // the selected row number is accessible as an integer
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let color = colors[pickerView.selectedRow(inComponent: 0)]
+        let type = types[pickerView.selectedRow(inComponent: 1)]
+                choiceDisplay.text = color + " " + type
+    }
+    
+    
+    @IBOutlet weak var flowerChoice: UIPickerView!
+    
+    @IBOutlet weak var choiceDisplay: UILabel!
 }
 
